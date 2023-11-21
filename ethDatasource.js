ccc14c1403e78a89fbf4e87b22f133a8be5a381c
Aman Dangol <53999580+amandangol@users.noreply.github.com>
2023-11-21 12:21:53 +0545

    Add files via upload

---
+++ ethDatasource.js
@@ -0,0 +1,28 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+//Vitalik's Ethereum Address
+const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+//Etherscan Data Source Class
+class EtherDataSource extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://api.etherscan.io/api";
+  }
+
+  async etherBalanceByAddress() {
+    return this.get(
+      `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  async totalSupplyOfEther() {
+    return this.get(
+      `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  //Paste Code Here For New API Endpoints
+}
+
+module.exports = EtherDataSource;
