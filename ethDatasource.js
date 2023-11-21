62b055cc4f0edbd3d3d0aec686467a246044487e
Aman Dangol <53999580+amandangol@users.noreply.github.com>
2023-11-21 12:34:28 +0545

    Add files via upload

--- ethDatasource.js
+++ ethDatasource.js
@@ -1,28 +1,46 @@
-const { RESTDataSource } = require("apollo-datasource-rest");
+// Import RESTDataSource from Apollo Server to enable REST API data sources
+const { RESTDataSource } = require("apollo-datasource-rest"); 
 
-//Vitalik's Ethereum Address
+// Ethereum address we will use in the API requests
 const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
 
-//Etherscan Data Source Class
+// Extend RESTDataSource to create a custom data source for Etherscan APIs
 class EtherDataSource extends RESTDataSource {
+
+  // Set the base URL for Etherscan API 
   constructor() {
     super();
     this.baseURL = "https://api.etherscan.io/api";
   }
 
+  // Get ETH balance for an Ethereum address  
   async etherBalanceByAddress() {
     return this.get(
       `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
     );
   }
-
+  
+  // Get total ETH supply
   async totalSupplyOfEther() {
     return this.get(
       `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
     );
   }
 
-  //Paste Code Here For New API Endpoints
+  // Get latest ETH price
+  async getLatestEthereumPrice() {
+    return this.get(
+      `?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  // Get estimated block confirmation time
+  async getBlockConfirmationTime() {
+    return this.get(
+      `?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
 }
 
+// Export EtherDataSource class
 module.exports = EtherDataSource;
--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-const { ApolloServer } = require("apollo-server");
+// Import Apollo Server and schema import utility
+const { ApolloServer } = require("apollo-server"); 
 const { importSchema } = require("graphql-import");
-const EtherDataSource = require("./datasource/ethDatasource");
-const typeDefs = importSchema("./schema.graphql");
 
+// Import custom data source 
+const EtherDataSource = require("./datasource/ethDatasource");
+
+// Import GraphQL schema
+const typeDefs = importSchema("./schema.graphql"); 
+
+// Load environment variables
 require("dotenv").config();
 
+// Create resolver map
 const resolvers = {
-  Query: {
-    getEthByAddress: (root, _args, { dataSources }) =>
-      dataSources.ethDataSource.etherBalanceByAddress(),
-    getTotalSupplyEth: (root, _args, { dataSources }) =>
-      dataSources.ethDataSource.totalSupplyOfEther(),
-    //Paste Code for New Resolver Functions
-  },
+ Query: {
+   // Resolver for etherBalanceByAddress query
+   etherBalanceByAddress: (root, _args, { dataSources }) =>  
+     dataSources.ethDataSource.etherBalanceByAddress(),
+
+   // Resolver for totalSupplyOfEther query   
+   totalSupplyOfEther: (root, _args, { dataSources }) =>
+     dataSources.ethDataSource.totalSupplyOfEther(),
+
+   // Resolver for latestEthereumPrice query
+   latestEthereumPrice: (root, _args, { dataSources }) =>
+     dataSources.ethDataSource.getLatestEthereumPrice(),
+
+   // Resolver for blockConfirmationTime query
+   blockConfirmationTime: (root, _args, { dataSources }) =>
+     dataSources.ethDataSource.getBlockConfirmationTime(),
+ },
 };
 
+// Create ApolloServer instance
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => ({
-    ethDataSource: new EtherDataSource(),
-  }),
+ typeDefs,
+ resolvers,
+ 
+ // Pass EtherDataSource instance to dataSources
+ dataSources: () => ({
+   ethDataSource: new EtherDataSource(), 
+ }),
+}); 
+
+// Disable request timeout
+server.timeout = 0;
+
+// Start the server
+server.listen("9000").then(({ url }) => {
+ console.log(`🚀 Server ready at ${url}`); 
 });
 
-server.timeout = 0;
-server.listen("9000").then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
